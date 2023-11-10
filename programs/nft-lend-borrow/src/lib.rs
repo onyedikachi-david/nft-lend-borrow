2b22b32f3e33b20cf56ee77a3c2ba0965d5372f8
David Anyatonwu <davidanyatonwu@gmail.com>
2023-11-10 08:52:42 +0100

    Done??

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -1,6 +1,8 @@
+pub mod errors;
 pub mod instructions;
 pub mod states;
 
+pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
@@ -20,6 +22,25 @@ pub mod nft_lend_borrow {
 
     pub fn offer_loan(ctx: Context<OfferLoan>, offer_amount: u64) -> Result<()> {
         instructions::offer_loan::handler(ctx, offer_amount)
+    }
+
+    pub fn withdraw_offer(
+        ctx: Context<WithdrawOffer>,
+        minimum_balance_for_rent_exemption: u64,
+    ) -> Result<()> {
+        instructions::withdraw_offer::handler(ctx, minimum_balance_for_rent_exemption)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, minimum_balance_for_rent_exemption: u64) -> Result<()> {
+        instructions::borrow::handler(ctx, minimum_balance_for_rent_exemption)
+    }
+
+    pub fn repay(ctx: Context<Repay>) -> Result<()> {
+        instructions::repay::handler(ctx)
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
+        instructions::liquidate::handler(ctx)
     }
 }
 
